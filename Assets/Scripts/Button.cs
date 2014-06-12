fed7cf3f22d90f8fe77352c9b34b7737d195a8cf
NiniMousse <gnbarbel@hotmail.fr>
2014-06-13 00:19:54 +0200

    Main menu

---
+++ Assets/Scripts/Button.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Button : MonoBehaviour
+{
+	public string NextScene = "";
+	
+	void OnMouseEnter()
+	{
+		renderer.material.color = Color.green;
+	}
+	
+	void OnMouseExit()
+	{
+		renderer.material.color = Color.white;
+	}
+	
+	void OnMouseDown()
+	{
+		print("wazaaaaaa");
+        if (!NextScene.Equals(""))
+            Application.LoadLevel(NextScene);
+        /*if (this.name.Equals("Return"))
+        {
+            Pause.SetActive(false);
+            RootInGame.SetActive(true);
+        }*/
+        if (this.name.Equals("Button_Quit"))
+        {
+            Application.Quit();
+        }
+	}
+}
