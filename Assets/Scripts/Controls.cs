17fc086d55ad834c234327cb4c74b3629a4790e0
leon196 <lowpolygonum@gmail.com>
2014-06-12 17:53:11 +0200

    Vertical Control (Gravity+Jump)

--- Assets/Scripts/Controls.cs
+++ Assets/Scripts/Controls.cs
@@ -1,13 +1,19 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
-public class Controls : MonoBehaviour {
+public class Controls : MonoBehaviour
+{
+	private List<Bounds> blocsBounds;
 
+	private bool ground = false;
 	private Vector3 input;
+
 	private Vector3 velocity;
 	private float speed = 1.5f;
+	private float jump = 0.275f;
 	private float drag = 0.75f;
-	private float gravity = 0.38f;
+	private float gravity = 0.0038f;
 	private const float VELOCITY_MAX = 0.1f;
 
 	// Use this for initialization
@@ -15,27 +21,53 @@ public class Controls : MonoBehaviour {
 	{
 		input = new Vector3();
 		velocity = new Vector3();
+
+		blocsBounds = Manager.Instance.BlocsBounds;
+		Debug.Log(blocsBounds.Count);
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate ()
 	{
+		// Inputs
 		input.x = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
 		input.y = Input.GetAxis("Vertical") * Time.deltaTime * speed;
 
+		// Collision
+		ground = false;
+		foreach (Bounds bounds in blocsBounds) {
+			if (renderer.bounds.Intersects(bounds)) {
+				ground = true;
+			}
+		}
+
+		// Velocity Horizontal
 		velocity.x = Mathf.Max(-VELOCITY_MAX, Mathf.Min(velocity.x + input.x, VELOCITY_MAX));
 
+		// Velocity Drag
 		if (Mathf.Abs(velocity.x) < 0.1) {
 			velocity.x *= drag;
 		}
 
-		//velocity.y += input.y;
+		// Velocity Vertical
 
+		// In Air
+		if (!ground)
+		{
+			velocity.y = Mathf.Max(-VELOCITY_MAX, Mathf.Min(velocity.y - gravity, VELOCITY_MAX));
+		}
+		// On Ground
+		else
+		{
+			if (input.y > 0.0f) {
+				velocity.y += jump;
+				ground = false;
+			} else {
+				velocity.y = 0.0f;
+			}
+		}
+
+		// Apply Transformations
 		transform.position += velocity;
-	}
-
-	void OnCollisionEnter (Collision other)
-	{
-
 	}
 }
---
+++ Assets/Scripts/Manager.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Manager : MonoBehaviour {
+
+	private static Manager _instance;
+
+	private Manager() {}
+
+	void Awake () {
+		_instance = this;
+	}
+
+	// Singleton
+	public static Manager Instance {
+		get  {
+			return _instance;
+		}
+	}
+
+	// Get Blocs Bounds
+	private List<Bounds> _blocsBounds;
+	public List<Bounds> BlocsBounds {
+		get {
+			if (_blocsBounds == null) {
+				_blocsBounds = new List<Bounds>();
+				SpriteRenderer[] sprites = GetComponentsInChildren<SpriteRenderer>();
+				foreach (SpriteRenderer sprite in sprites) {
+					if (sprite.name == "Bloc") {
+						_blocsBounds.Add(sprite.bounds);
+					}
+				}
+			}
+			return _blocsBounds;
+		}
+	}
+}
