b3eb3b3ca4cc1235a20acf632a70fd796cfe1d8b
Leon Denise <lowpolygonum@gmail.com>
2014-06-13 12:56:50 +0200

    Background Helicopter Animation

--- Assets/Scripts/Animation.cs
+++ Assets/Scripts/Animation.cs
@@ -6,6 +6,8 @@ public class Animation : MonoBehaviour {
 	public bool turn = true;
 	public float radiusTurn = 3.0f;
 	public float speedTurn = 1.0f;
+	public bool orientation = true;
+	private float lastX = 0.0f;
 
 	public bool translate = false;
 	public float radiusTranslate = 4.0f;
@@ -32,5 +34,15 @@ public class Animation : MonoBehaviour {
 		}
 
 		transform.position = new Vector3(x, y, 0f);
+
+		if (orientation) {
+			if (lastX > 0.0f && x < 0.0f) {
+				transform.localScale = new Vector3(-1f, 1f, 1f);
+			} else if (lastX < 0.0f && x > 0.0f) {
+				transform.localScale = new Vector3(1f, 1f, 1f);
+			}
+		}
+
+		lastX = x;
 	}
 }
