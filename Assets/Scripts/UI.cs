fa1cb3fcec269bc8773cc0005fbdaff5166c5218
NiniMousse <gnbarbel@hotmail.fr>
2014-06-13 10:35:37 +0200

    Timer

---
+++ Assets/Scripts/UI.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class UI : MonoBehaviour {
+	bool display_GO = true;
+	float startTime;
+	string timerText;
+
+	void Awake() {	
+		startTime = Time.time;
+		
+	}
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnGUI()
+	{
+		float guiTime = Time.time - startTime;
+		
+		float minutes = guiTime / 60f;
+		float seconds = guiTime % 60f;
+		float fraction = (guiTime * 100f) % 100f;
+		
+		timerText = String.Format ("{0:00}:{1:00}:{2:000}", minutes, seconds, fraction); 
+		GUI.Label (new Rect (400, 25, 100, 30), timerText);
+	}
+}
